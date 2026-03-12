fa5060b383b625711312dbebe912d16b7cd6d15c
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-03-12 16:20:33 -0500

    feat: Attach reveal observer to cards and footer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./styles.css"
+
+function revealSection() {
+    const projectCards = document.querySelectorAll(".project-card");
+    const footer = document.getElementById("contact-section");
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting && entry.intersectionRatio >= 0.75) {
+                entry.target.classList.add("show");
+                observer.unobserve(entry.target);
+            }
+        })
+    }, {threshold: 0.75})
+
+    projectCards.forEach((card) => {
+        observer.observe(card);
+    })
+
+    observer.observe(footer);
+}
+
+revealSection();
